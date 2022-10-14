a6ad8c66d9f97def807c51c5e351f157d935d1a4
Admin <Admin@DESKTOP-57MEOSK>
2022-10-14 14:05:37 -0600

    Agregar archivos de proyecto.

---
+++ L10_RAJCL_1073522/L10_RAJCL_1073522.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace L10_RAJCLA_1073522
+{
+
+    class Program
+    {
+
+        string usuario;
+        string contraseña;
+
+        public static string Login(string usuario, string contraseña)
+        {
+            string NombreUsuario = "usuario1";
+            return NombreUsuario;
+
+            string Contraseña = "asdasd";
+            return Contraseña;
+        }
+
+        static void Main(string[] args, string contraseña, string usuario)
+        {
+            Console.WriteLine("Ingrese sus datos de Login");
+            Console.WriteLine("Nombre de Usuario");
+            usuario = Console.ReadLine();
+
+            Console.WriteLine("Contraseña");
+            contraseña = Console.ReadLine();
+        }
+        
+
+       
+    }
+
+}
---
+++ L10_RAJCL_1073522/Program.cs
@@ -0,0 +1,49 @@
+﻿ class Program
+{
+    static void Main(string[] args)
+    {
+        string usuario;
+        string contraseña;
+        int x = 0;
+
+        Boolean datos;
+        do
+        {
+            Console.WriteLine("Ingrese su usuario");
+            usuario = Console.ReadLine();
+            Console.WriteLine("Ingrese su contraseña");
+            contraseña = Console.ReadLine();
+
+            datos = Login(usuario, contraseña);
+            if (datos == true)
+            {
+                Console.WriteLine("Datos correctos, BIENVENIDO");
+                x = 3;
+            }
+            else
+            {
+                x++;
+                Console.WriteLine("Datos incorrectos, INTENTELO NUEVAMENTE");
+                Console.WriteLine("INTENTO NÚMERO " + x);
+            }
+        } while (x < 3);
+        Console.ReadKey();
+    }
+    static bool Login(string usuario, string contraseña)
+    {
+
+        if (usuario== "usuario1" && contraseña == "asdasd")
+        {
+            return true;
+        }
+        else
+        {
+
+            return false;
+        }
+
+
+    }
+
+}
+
